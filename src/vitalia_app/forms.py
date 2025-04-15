bcf363df69e650f367da938ecf6c7ef8de1723a7
Mathéo Arondeau <matheo.arondeau@gmail.com>
2025-04-15 18:39:57 +0200

    formulaire de contact fonctionnel

--- src/vitalia_app/forms.py
+++ src/vitalia_app/forms.py
@@ -1,7 +1,16 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    nom = forms.CharField(max_length=100)
-    email = forms.EmailField()
-    message = forms.CharField(widget=forms.Textarea)
+    OBJET_CHOICES = [
+        ('infos', 'Demande d\'informations'),
+        ('visite', 'Demande de visite'),
+        ('disponibilite', 'Demande de disponibilités'),
+        ('recrutement', 'Candidature / Recrutement'),
+        ('partenariat', 'Demande de partenariat'),
+        ('autre', 'Autre demande'),
+    ]
 
+    nom = forms.CharField(max_length=100, label="Nom complet")
+    email = forms.EmailField(label="Adresse e-mail")
+    objet = forms.ChoiceField(choices=OBJET_CHOICES, label="Objet de la demande")
+    message = forms.CharField(widget=forms.Textarea, label="Votre message")
