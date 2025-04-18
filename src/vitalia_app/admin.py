4a6e6c825a74e8c6e75de8633cd2c5d21043d819
Relkha <elkharr@cy-tech.fr>
2025-04-18 15:47:41 +0200

    Ajout de la gestions des permissions en fonction de chaque groupe dans l'admin de django

--- src/vitalia_app/admin.py
+++ src/vitalia_app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import GroupAdmin
+from django.contrib.auth.models import Group
 from .models.Profil import Profil
 
 
@@ -28,3 +30,39 @@ class UserAdmin(BaseUserAdmin):
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
+
+from django.contrib import admin
+from .models.Objets import ConnectedObject, PermissionType, ObjectPermission
+
+@admin.register(PermissionType)
+class PermissionTypeAdmin(admin.ModelAdmin):
+    list_display = ('code', 'label')
+    search_fields = ('code', 'label')
+
+@admin.register(ConnectedObject)
+class ConnectedObjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'status', 'room')
+    list_filter = ('type', 'status')
+    search_fields = ('name', 'description', 'room')
+
+@admin.register(ObjectPermission)
+class ObjectPermissionAdmin(admin.ModelAdmin):
+    list_display = ('connected_object', 'group')
+    list_filter = ('group',)
+    search_fields = ('connected_object__name', 'group__name')
+    filter_horizontal = ('permissions',)  # Permet de sélectionner les permissions plus facilement
+
+class ObjectPermissionInline(admin.TabularInline):
+    model = ObjectPermission
+    extra = 0
+    verbose_name = "Objet connecté autorisé"
+    verbose_name_plural = "Objets accessibles"
+    filter_horizontal = ("permissions",)
+    fk_name = "group"
+
+class CustomGroupAdmin(GroupAdmin):
+    inlines = [ObjectPermissionInline]
+
+# Remplacer l'ancien Group admin par le nouveau
+admin.site.unregister(Group)
+admin.site.register(Group, CustomGroupAdmin)
